1bd88c846f3c593de2e836afa497d7fb16bcae57
Gonzalez Pugantell Facundo <fgonzalezpugantell@gmail.com>
2023-10-12 18:09:03 -0300

    Primera Pre Entrega

--- ProyectoReact/src/components/saludo/index.jsx
+++
@@ -1,5 +0,0 @@
-const Saludo = ({ nombre }) => {
-  return <p>Mensaje 1</p>;
-};
-
-export default Saludo;
---
+++ ProyectoReact/src/containers/ItemListContainer/ItemListContainer.jsx.jsx
@@ -0,0 +1,9 @@
+const ItemListContainer = ({ greeting }) => {
+  return (
+    <>
+      <h2 classname="Saludo"> {greeting} </h2>
+    </>
+  );
+};
+
+export default ItemListContainer;
